2f6eb16706589676a5c258bf15bb0a6f77fac446
Alessandro Petrini <apetrini@horizenlabs.io>
2023-05-23 17:36:51 +0200

    Add example test files

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,24 @@
 // sha256: c03905fcdab297513a620ec81ed46ca44ddb62d41cbbd83eb4a5a3592be26a69
 // sha512: a12ac6bdd854ac30c5cc5b576e1ee2c060c0d8c2bec8797423d7119aa2b962f7f30ce2e39879cbff0109c8f0a3fd9389a369daae45df7d7b286d7d98272dc5b1
 // 
-// test.txt
+// assets/test.txt
 // --------
 // sha1sum:   be417768b5c3c5c1d9bcb2e7c119196dd76b5570  test.txt
 // sha256sum: c03905fcdab297513a620ec81ed46ca44ddb62d41cbbd83eb4a5a3592be26a69  test.txt
 // sha512sum: a12ac6bdd854ac30c5cc5b576e1ee2c060c0d8c2bec8797423d7119aa2b962f7f30ce2e39879cbff0109c8f0a3fd9389a369daae45df7d7b286d7d98272dc5b1  test.txt
 //
-// 100M.txt
+// assets/100M.txt
 // --------
 // sha1sum:   cd8c10d7a27e7b73d37680e54357e5d54e8d330d  100M.txt
 // sha256sum: 5ebc45fd76da5fefc13a21d5ce3a86fdab7653d0c0e3ce98a65be108f3896d1d  100M.txt
 // sha512sum: 7c201e142d973538d6fa7be43d5f00a9684c35d4117d98ba191b785ab77da0574451019878bd21b28aab1a402e04726d69e9f5bd335fa7a3f80877693629de6c  100M.txt
+//
+// assets/4M.txt
+// --------
+// sha1sum:   d851111465cb4829cb96d12b5b13194430e75d02  4M.txt
+// sha256sum: 428b2e5b1496cf2d56bcc94648c6398600e44f8a5105a86af93e577df88e146a  4M.txt
+// sha512sum: ae7b4027f4a74fe07874b12ae2fc47e92a83c81f50b68f18793a401def835695a4077094ab38c13bb42586f425cffbbbc8bc85d1abeaa467785f3ef97b03ac95  4M.txt
+
 //////
 
 // Naaaaa, mannaggia! Globali!
