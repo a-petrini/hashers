9223141737d5a6c3734dc21244d0fa68aee803a7
Alessandro Petrini <apetrini@horizenlabs.io>
2023-05-24 10:38:50 +0200

    Write and Finalize calls changed to adhere to repo convention

--- src/zcash_hashers.cpp
+++ src/zcash_hashers.cpp
@@ -12,25 +12,18 @@
 
 std::vector<unsigned char> zcash_sha1_hasher(const std::vector<char> & filebuffer) {
     std::vector<unsigned char> zcashSha1Output(zcash::CSHA1::OUTPUT_SIZE); // 20
-    zcash::CSHA1 zcashSha1Hasher;
-    zcashSha1Hasher = zcashSha1Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zcashSha1Hasher.Finalize(zcashSha1Output.data());
+    zcash::CSHA1().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zcashSha1Output.data());
     return zcashSha1Output;
 }
 
 std::vector<unsigned char> zcash_sha256_hasher(const std::vector<char> & filebuffer) {
     std::vector<unsigned char> zcashSha256Output(zcash::CSHA256::OUTPUT_SIZE); // 32
-    zcash::CSHA256 zcashSha256Hasher;
-    zcashSha256Hasher = zcashSha256Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zcashSha256Hasher.Finalize(zcashSha256Output.data());
+    zcash::CSHA256().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zcashSha256Output.data());
     return zcashSha256Output;
-
 }
 
 std::vector<unsigned char> zcash_sha512_hasher(const std::vector<char> & filebuffer) {
     std::vector<unsigned char> zcashSha512Output(zcash::CSHA512::OUTPUT_SIZE); // 64
-    zcash::CSHA512 zcashSha512Hasher;
-    zcashSha512Hasher = zcashSha512Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zcashSha512Hasher.Finalize(zcashSha512Output.data());
+    zcash::CSHA512().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zcashSha512Output.data());
     return zcashSha512Output;
 }
--- src/zen_hashers.cpp
+++ src/zen_hashers.cpp
@@ -10,29 +10,20 @@
 #include "zen/sha256.h"
 #include "zen/sha512.h"
 
-
 std::vector<unsigned char> zen_sha1_hasher(const std::vector<char> & filebuffer) {
     std::vector<unsigned char> zenSha1Output(zen::CSHA1::OUTPUT_SIZE); // 20
-    zen::CSHA1 zenSha1Hasher;
-    zenSha1Hasher = zenSha1Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zenSha1Hasher.Finalize(zenSha1Output.data());
+    zen::CSHA1().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zenSha1Output.data());
     return zenSha1Output;
 }
 
 std::vector<unsigned char> zen_sha256_hasher(const std::vector<char> & filebuffer) {
-    //// zen SHA-256 hasher
     std::vector<unsigned char> zenSha256Output(zen::CSHA256::OUTPUT_SIZE); // 32
-    zen::CSHA256 zenSha256Hasher;
-    zenSha256Hasher = zenSha256Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zenSha256Hasher.Finalize(zenSha256Output.data());
+    zen::CSHA256().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zenSha256Output.data());
     return zenSha256Output;
 }
 
 std::vector<unsigned char> zen_sha512_hasher(const std::vector<char> & filebuffer) {
-    //// zen SHA-512 hasher
     std::vector<unsigned char> zenSha512Output(zen::CSHA512::OUTPUT_SIZE); // 64
-    zen::CSHA512 zenSha512Hasher;
-    zenSha512Hasher = zenSha512Hasher.Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size());
-    zenSha512Hasher.Finalize(zenSha512Output.data());
+    zen::CSHA512().Write(reinterpret_cast<const unsigned char*>(filebuffer.data()), filebuffer.size()).Finalize(zenSha512Output.data());
     return zenSha512Output;
 }
